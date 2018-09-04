07c631f1dbeb4781b15749bb9e4bd673b74292c5
Carson Williams <sxhatrix@gmail.com>
2018-09-04 16:47:10 -0600

    makes log readable

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ const roll = (message) => {
 }
 
 client.on("message", (message) => {
-  console.log(message.author.username + ": " + message.content);
+  // log message [servername] [channelname] author: msg
+  console.log("[" + message.guild + "] [" + message.channel.name + "] " + message.author.username + ": " + message.content);
 
   if (message.author.bot) return; // ignore all bot messages
 
