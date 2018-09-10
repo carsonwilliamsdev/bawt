b2c99ae22e4fd21b775cbb63bab05c637a85bf6a
boveus <boveus@gmail.com>
2018-09-10 19:50:28 -0400

    add limit 10000

--- reddit_quote.js
+++ reddit_quote.js
@@ -6,7 +6,8 @@ module.exports = {
    newQuote: function() {
       return  (async () => {
         let user = 'beeyayzah'
-        let feed = await parser.parseURL(`http://www.reddit.com/user/${user}/comments/.rss`);
+        let limit = 10000
+        let feed = await parser.parseURL(`http://www.reddit.com/user/${user}/comments/.rss?limit=${limit}`);
         var item = feed.items[Math.floor(Math.random()*feed.items.length)];
         let quote = item.contentSnippet
         return quote
