90a796ec5caac85032b40184768f00566c859a99
Brandon Stewart <boveus@gmail.com>
2019-02-22 18:20:53 -0500

    fix define

--- google-define.js
+++ google-define.js
@@ -1,17 +1,18 @@
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
 let defineWord = function (query) {
-  console.log(query)
   return fetch(`https://googledictionaryapi.eu-gb.mybluemix.net?define=${query['word']}&lang=en`)
   .then(function(response) {
       return response.json()
   })
   .then(function(json)  {
-    let result = json[0]
     if (query.typeOfWord) {
-      definition = result.meaning[query.typeOfWord][0].definition
+      return json.meaning[query.typeOfWord][0].definition
     } else {
-      definition = result.meaning['noun'][0].definition
+      return json.meaning['noun'][0].definition
     }
-    return definition
  })
 }
 
